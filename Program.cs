daa179178799f3d6c832e0e7dc7f2296511642c7
Bora Saglam <posarobit@gmail.com>
2025-08-01 22:45:13 +0300

    Yeni repo açıldı

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿int sayi1 = 800;
+int sayi2 = 50;
+int sayi3 = 45;
+int sayi4 = 34;
+
+int toplam = sayi1 + sayi2;
+
+int cikarma = sayi1 - sayi3;
+
+int carpma = sayi3 * sayi4;
+
+int bolme = sayi1 / sayi4;
+
+int islem1 = 8 * 8;
+
+int islem2 = 8 * 8 * 8;
+
+
+Console.WriteLine($"Toplam işleminin sonucu: {toplam}");
+
+Console.WriteLine($"Çıkarma işleminin sonucu: {cikarma}");
+
+Console.WriteLine($"Çarpma işleminin sonucu: {carpma}");
+
+Console.WriteLine($"Bölme işleminin sonucu: {bolme}");
+
+Console.WriteLine($"İşlem 1 in işleminin sonucu: {islem1}");
+
+Console.WriteLine($"İşlem 2 in işleminin sonucu: {islem2}");
+
+
+Console.Write($"Adınız: ");
+string name = Console.ReadLine();
+
+
+Console.Write("Soyadınız: ");
+string surname = Console.ReadLine();
+
+
+Console.Write("Yaşınız: ");
+string yas = Console.ReadLine();
+
+
+Console.Write("Yaşadığınız il: ");
+string yasadiginiz_il = Console.ReadLine();
+
+
+
+Console.WriteLine($"Adınız: " + name);
+Console.WriteLine($"Soyadınız: " + surname);
+Console.WriteLine($"Yaşınız: " + yas);
+Console.WriteLine($"Yaşadığınız il: " + yasadiginiz_il);
+
+
+
+ 
+
+
+
